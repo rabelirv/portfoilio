9fbab6e09160e269553042b2c3e8d1741d67979f
Your Name <rabel606@gmail.com>
2021-02-16 16:17:48 -0500

    Added Menu Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className={classes.root}>
+    <AppBar position="static" color= "white">
+    <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+    </AppBar>
+      <Typography variant="h2" className={classes.title}>Hi, I'm Irving Rabel, a Software Engineer</Typography>
+      <Typography variant="h3" className={classes.title}>Welcome to my O A S I S </Typography>
     </div>
   );
 }
